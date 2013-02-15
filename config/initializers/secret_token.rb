ebcf6a4a0c5f3aad9cc2934c9d30dd0b6977fb93
Georgina Edwards <gc_edwards@yahoo.com.au>
2013-02-15 15:09:52 +1100

    Basic blacklight example app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlSample::Application.config.secret_token = '809bf5d0873ae9d3e85c0ead47cc8ddfc2d43d11e873377e1bf6d6edb2df6253589b08191c97726dbabab1ec58ad94428ee77a8313d18a44211e00f19e6b3ce2'
